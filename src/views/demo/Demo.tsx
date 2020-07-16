4f942add486f211ecd775b3f44cecb719795ca3a
ekko <ekko.cui@gmail.com>
2020-07-16 17:56:19 +0800

    初始化antd-demo-ts

---
+++ src/views/demo/Demo.tsx
@@ -0,0 +1,19 @@
+import React, { FC } from 'react';
+import Nav from './../nav/Nav';
+
+
+const Demo: FC = () => {
+    return (
+        <div>
+            <Nav />
+            <span>hello</span>
+        </div>
+    );
+}
+
+
+export default Demo;
+
+
+
+
